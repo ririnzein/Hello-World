0a32b74e2e6ca731585e002f1f21b94001c5b397
ririnzein <57787339+ririnzein@users.noreply.github.com>
2019-11-26 19:35:51 +0700

    commit

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf (" Hello ririn!");
+    int angka = 7;
+    printf("Variabel dengan nilai %d\n", angka);
+
+    int huruf = 10;
+    printf("varianbel dengan nilai %d\n, huruf");
+
+
     return 0;
 }
---
+++ soal sendiri 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+    int angka = 10;
+    printf("variabel dengan nilai %d\n", angka);
+
+    int huruf = 20;
+    printf("variabel dengan nilai %d\n", huruf);
+
+    int siswa = 35;
+    printf("variabel dengan nilai %d\n", siswa);
+
+    int kursi = 25;
+    printf("variabel dengan nilai %d\n", kursi);
+
+    return 0;
+}
